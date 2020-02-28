8853b7aebe9e113d89552173ba5b46bee04bc1ee
Restyled.io <commits@restyled.io>
2020-02-28 04:15:41 +0000

    Restyled by clang-format

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,51 +1,51 @@
 /*
  * Copyright (c) 2017 American Express Travel Related Services Company, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
  */
 
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import React, {Component} from 'react';
 
 class Steps extends Component {
   componentDidMount() {
-    const steps = React.Children.map(
-      this.props.children,
-      ({ props: { children, render, ...config } }) => config
-    );
+    const steps =
+        React.Children.map(this.props.children,
+                           ({props : {children, render, ...config}}) => config);
     this.context.wizard.init(steps);
   }
 
   render() {
-    const { id: activeId } = this.props.step || this.context.wizard.step;
-    const [child = null] = React.Children.toArray(this.props.children).filter(
-      ({ props: { id } }) => id === activeId
-    );
+    const {id : activeId} = this.props.step || this.context.wizard.step;
+    const [child = null] = React.Children.toArray(this.props.children)
+                               .filter(({props : {id}}) => id === activeId);
     return child;
   }
 }
 
 Steps.propTypes = {
-  children: PropTypes.node.isRequired,
-  step: PropTypes.shape({
-    id: PropTypes.string.isRequired,
+  children : PropTypes.node.isRequired,
+  step : PropTypes.shape({
+    id : PropTypes.string.isRequired,
   }),
 };
 
 Steps.defaultProps = {
-  step: null,
+  step : null,
 };
 
 Steps.contextTypes = {
-  wizard: PropTypes.object,
+  wizard : PropTypes.object,
 };
 
 export default Steps;
--- src/components/Wizard.js
+++ src/components/Wizard.js
@@ -1,71 +1,65 @@
 /*
  * Copyright (c) 2017 American Express Travel Related Services Company, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
  */
 
-import { Component } from 'react';
+import {createMemoryHistory} from 'history';
 import PropTypes from 'prop-types';
-import { createMemoryHistory } from 'history';
+import {Component} from 'react';
+
 import renderCallback from '../utils/renderCallback';
 
 class Wizard extends Component {
   state = {
-    step: {
-      id: null,
+    step : {
+      id : null,
     },
     steps: [],
   };
 
   getChildContext() {
     return {
-      wizard: {
-        go: this.history.go,
-        history: this.history,
-        init: this.init,
-        next: this.next,
-        previous: this.history.goBack,
-        push: this.push,
-        replace: this.replace,
+      wizard : {
+        go : this.history.go,
+        history : this.history,
+        init : this.init,
+        next : this.next,
+        previous : this.history.goBack,
+        push : this.push,
+        replace : this.replace,
         ...this.state,
       },
     };
   }
 
   componentDidMount() {
-    this.unlisten = this.history.listen(({ pathname }) =>
-      this.setState({ step: this.pathToStep(pathname) })
-    );
+    this.unlisten = this.history.listen(
+        ({pathname}) => this.setState({step : this.pathToStep(pathname)}));
 
     if (this.props.onNext) {
-      const { init, ...wizard } = this.getChildContext().wizard;
+      const {init, ...wizard} = this.getChildContext().wizard;
       this.props.onNext(wizard);
     }
   }
 
-  componentWillUnmount() {
-    this.unlisten();
-  }
+  componentWillUnmount() { this.unlisten(); }
 
-  get basename() {
-    return `${this.props.basename}/`;
-  }
+  get basename() { return `${this.props.basename}/`; }
 
-  get ids() {
-    return this.state.steps.map(s => s.id);
-  }
+  get ids() { return this.state.steps.map(s => s.id); }
 
-  get nextStep() {
-    return this.ids[this.ids.indexOf(this.state.step.id) + 1];
-  }
+  get nextStep() { return this.ids[this.ids.indexOf(this.state.step.id) + 1]; }
 
   history = this.props.history || createMemoryHistory();
   steps = [];
@@ -73,17 +67,16 @@ class Wizard extends Component {
   pathToStep = pathname => {
     const id = pathname.replace(this.basename, '');
     const [step] = this.state.steps.filter(
-      s => (this.props.exactMatch ? s.id === id : id.startsWith(s.id))
-    );
+        s => (this.props.exactMatch ? s.id === id : id.startsWith(s.id)));
 
     return step || this.state.step;
   };
 
   init = steps => {
-    this.setState({ steps }, () => {
+    this.setState({steps}, () => {
       const step = this.pathToStep(this.history.location.pathname);
       if (step.id) {
-        this.setState({ step });
+        this.setState({step});
       } else {
         this.history.replace(`${this.basename}${this.ids[0]}`);
       }
@@ -91,7 +84,8 @@ class Wizard extends Component {
   };
 
   push = (step = this.nextStep) => this.history.push(`${this.basename}${step}`);
-  replace = (step = this.nextStep) => this.history.replace(`${this.basename}${step}`);
+  replace = (step = this.nextStep) =>
+      this.history.replace(`${this.basename}${step}`);
 
   next = () => {
     if (this.props.onNext) {
@@ -102,36 +96,36 @@ class Wizard extends Component {
   };
 
   render() {
-    const { init, ...wizard } = this.getChildContext().wizard;
+    const {init, ...wizard} = this.getChildContext().wizard;
     return renderCallback(this.props, wizard);
   }
 }
 
 Wizard.propTypes = {
-  basename: PropTypes.string,
-  history: PropTypes.shape({
-    entries: PropTypes.array,
-    go: PropTypes.func,
-    goBack: PropTypes.func,
-    listen: PropTypes.func,
-    location: PropTypes.object,
-    push: PropTypes.func,
-    replace: PropTypes.func,
+  basename : PropTypes.string,
+  history : PropTypes.shape({
+    entries : PropTypes.array,
+    go : PropTypes.func,
+    goBack : PropTypes.func,
+    listen : PropTypes.func,
+    location : PropTypes.object,
+    push : PropTypes.func,
+    replace : PropTypes.func,
   }),
-  onNext: PropTypes.func,
-  exactMatch: PropTypes.bool,
+  onNext : PropTypes.func,
+  exactMatch : PropTypes.bool,
 };
 
 Wizard.defaultProps = {
-  basename: '',
-  history: null,
-  onNext: null,
-  render: null,
-  exactMatch: true,
+  basename : '',
+  history : null,
+  onNext : null,
+  render : null,
+  exactMatch : true,
 };
 
 Wizard.childContextTypes = {
-  wizard: PropTypes.object,
+  wizard : PropTypes.object,
 };
 
 export default Wizard;
